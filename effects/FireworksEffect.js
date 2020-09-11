48548a12b53ccdfababd78e056a399bc9841dcaf
Jeff Channell <me@jeffchannell.com>
2020-09-11 15:59:48 -0400

    added dispose

--- effects/FireworksEffect.js
+++ effects/FireworksEffect.js
@@ -101,6 +101,7 @@ const FireworksDrawingArea = GObject.registerClass({
             context.rectangle(spark.x, spark.y, 4, 4);
             context.fill();
         })));
+        context.$dispose();
     }
 
     render() {
