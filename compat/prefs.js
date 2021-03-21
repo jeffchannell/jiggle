bbabce70a9c98f514a6c1661e639dbd8cbf54c88
Jeff Channell <me@jeffchannell.com>
2021-03-21 01:49:08 -0400

    proper version checking

--- compat/prefs.js
+++ compat/prefs.js
@@ -2,36 +2,37 @@
 
 const {Gtk, GObject} = imports.gi;
 
-// Override Gtk.Box for cross-compat with Gtk3 and Gtk4
-const JiggleBox = GObject.registerClass({
-    GTypeName: 'JiggleBox',
-}, class JiggleBox extends Gtk.Box {
-    _init(params = {}) {
-        if ('function' !== typeof super.pack_start) {
+const shellVersion = Number(parseInt(imports.misc.config.PACKAGE_VERSION.split('.')[0]));
+
+let JiggleBox;
+
+if (40 >= shellVersion) {
+    JiggleBox = GObject.registerClass({
+        GTypeName: 'JiggleBox',
+    }, class JiggleBox extends Gtk.Box {
+        _init(params = {}) {
             if ('undefined' !== typeof params.border_width) {
                 delete params.border_width;
             }
+            super._init(params);
         }
-        super._init(params);
-    }
 
-    add(child) {
-        if ('function' == typeof super.add) {
-            return super.add(child);
+        add(child) {
+            super.append(child);
         }
-        return super.append(child);
-    }
 
-    pack_start(child, expand, fill, padding) {
-        if ('function' == typeof super.pack_start) {
-            return super.pack_start(child, expand, fill, padding);
+        pack_start(child, expand, fill, padding) {
+            super.append(child);
         }
-        return super.append(child);
-    }
 
-    show_all() {
-        if ('function' == typeof super.show_all) {
-            super.show_all();
+        show_all() {}
+    });
+} else {
+    JiggleBox = GObject.registerClass({
+        GTypeName: 'JiggleBox',
+    }, class JiggleBox extends Gtk.Box {
+        _init(params = {}) {
+            super._init(params);
         }
-    }
-});
+    });
+}
