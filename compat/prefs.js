028d79c5e03c393c78ed9646074e38fee832624e
Jeff Channell <me@jeffchannell.com>
2021-03-21 01:29:35 -0400

    use overrides for boxes

--- compat/prefs.js
+++ compat/prefs.js
@@ -1,35 +1,37 @@
 'use strict';
 
-const {Gtk} = imports.gi;
+const {Gtk, GObject} = imports.gi;
 
-const JiggleBox = class JiggleBox {
-    constructor(opts) {
-        // TODO how can we detect gtk3 vs gtk4?? hmmmmm
-        if ('undefined' !== typeof opts.border_width) {
-            delete opts.border_width;
+// Override Gtk.Box for cross-compat with Gtk3 and Gtk4
+const JiggleBox = GObject.registerClass({
+    GTypeName: 'JiggleBox',
+}, class JiggleBox extends Gtk.Box {
+    _init(params = {}) {
+        if ('function' !== typeof super.pack_start) {
+            if ('undefined' !== typeof params.border_width) {
+                delete params.border_width;
+            }
         }
-        this.box = new Gtk.Box(opts);
+        super._init(params);
     }
 
     add(child) {
-        if ('undefined' == typeof this.box.add) {
-            this.box.append(child);
-        } else {
-            this.box.add(child);
+        if ('function' == typeof super.add) {
+            return super.add(child);
         }
+        return super.append(child);
     }
 
     pack_start(child, expand, fill, padding) {
-        if ('undefined' == typeof this.box.pack_start) {
-            this.box.append(child);
-        } else {
-            this.box.pack_start(child, expand, fill, padding);
+        if ('function' == typeof super.pack_start) {
+            return super.pack_start(child, expand, fill, padding);
         }
+        return super.append(child);
     }
 
     show_all() {
-        if ('function' == typeof this.box.show_all) {
-            this.box.show_all();
+        if ('function' == typeof super.show_all) {
+            super.show_all();
         }
     }
-}
+});
